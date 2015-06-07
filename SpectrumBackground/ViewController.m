28d58f26c916cf421b4aafe69dbb90696753fae1
Mark Hall <golfguru@icloud.com>
2015-06-06 22:33:50 -0400

    calculating the coorditnates is better now

--- SpectrumBackground/ViewController.m
+++ SpectrumBackground/ViewController.m
@@ -24,11 +24,9 @@
     // Dispose of any resources that can be recreated.
 }
 - (IBAction)didPan:(UIPanGestureRecognizer *)gesture {
-    NSLog(@"%f, %f", [gesture locationInView:self.view].x, [gesture locationInView:self.view].y);
-    CGFloat x = [gesture locationInView:self.view].x;
-    CGFloat y = [gesture locationInView:self.view].y;
-    NSInteger intX = (int)x % 255;
-    NSInteger intY = (int)y % 255;
+    CGFloat x = [gesture locationInView:self.view].x / self.view.frame.size.width * 255;
+    CGFloat y = [gesture locationInView:self.view].y / self.view.frame.size.height * 255;
+    NSLog(@"%f, %f", x, y);
     self.view.backgroundColor = [UIColor colorWithRed:x/255.0 green:y/255.0 blue:(x+y)/255.0 alpha:1.0];
     
 }
