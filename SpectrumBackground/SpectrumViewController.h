3d25938eef27cbab795cd8190ee4b15bab2fe81f
Mark Hall <golfguru@icloud.com>
2015-06-10 21:19:52 -0400

    more fixing, added a gif

--- SpectrumBackground/SpectrumViewController.h
+++ SpectrumBackground/SpectrumViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  SpectrumViewController.h
 //  SpectrumBackground
 //
 //  Created by Mark Hall on 2015-06-06.
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface SpectrumViewController : UIViewController
 
 
 @end
--- SpectrumBackground/SpectrumViewController.m
+++ SpectrumBackground/SpectrumViewController.m
@@ -1,21 +1,29 @@
 //
-//  ViewController.m
+//  SpectrumViewController.m
 //  SpectrumBackground
 //
 //  Created by Mark Hall on 2015-06-06.
 //  Copyright (c) 2015 Mark Hall. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "SpectrumViewController.h"
+#define ARC4RANDOM_MAX      0x100000000
 
-@interface ViewController ()
+
+@interface SpectrumViewController ()
 
 @end
 
-@implementation ViewController
+@implementation SpectrumViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(didPan:)];
+    [self.view addGestureRecognizer:panGesture];
+    CGFloat x = (double)arc4random() / ARC4RANDOM_MAX;
+    CGFloat y = (double)arc4random() / ARC4RANDOM_MAX;
+    NSLog(@"%f, %f", x, y);
+    self.view.backgroundColor = [UIColor colorWithHue:x saturation:y brightness:1 alpha:1];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,7 +31,7 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-- (IBAction)didPan:(UIPanGestureRecognizer *)gesture {
+- (void)didPan:(UIPanGestureRecognizer *)gesture {
     CGFloat x = [gesture locationInView:self.view].x / self.view.frame.size.width;
     CGFloat y = [gesture locationInView:self.view].y / self.view.frame.size.height;
     y = y + 0.5;
